1d06f6f44e0e22b73f6ebc8d38106ce87dbdb78a
Kabidoye-17 <23365412@studentmail.ul.ie>
2025-05-20 18:16:50 +0100

    take in file from ui

--- src/desktop-client/client-404/mainwindow.cpp
+++ src/desktop-client/client-404/mainwindow.cpp
@@ -1,19 +1,45 @@
-#include <QLabel>
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    QLabel *label = new QLabel("Hello, World!", this);
-    label->setAlignment(Qt::AlignCenter);
-    setCentralWidget(label);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_uploadButton_clicked()
+{
+        QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "All Files (*.*)");
+
+        if (!filePath.isEmpty()) {
+            QFileInfo fileInfo(filePath);
+
+            QString fileName = fileInfo.fileName();
+            qint64 fileSize = fileInfo.size();  // originally in bytes
+            QString fileType = fileInfo.suffix();
+            QDateTime lastModified = fileInfo.lastModified();
+            QDateTime uploadTime = QDateTime::currentDateTime();
+
+            QString info = QString(
+                               "File Name: %1\n"
+                               "File Size: %2 bytes\n"
+                               "File Type: %3\n"
+                               "Last Modified: %4\n"
+                               "Upload Time: %5")
+                               .arg(fileName)
+                               .arg(fileSize)
+                               .arg(fileType)
+                               .arg(lastModified.toString("yyyy-MM-dd hh:mm:ss"))
+                               .arg(uploadTime.toString("yyyy-MM-dd hh:mm:ss"));
+
+            ui->textEdit->setText(info);
+        }
+}
+
