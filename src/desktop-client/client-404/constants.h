48f2dc0b6feebe8570fcca80e4a1b81cb706d7c7
Kabidoye-17 <23365412@studentmail.ul.ie>
2025-05-29 18:52:19 +0100

    make view page be more profesh

--- src/desktop-client/client-404/constants.h
+++ src/desktop-client/client-404/constants.h
@@ -8,12 +8,20 @@ const QString keysPath = "/encryptedKeys_";
 const QString masterKeyPath = "/masterKey_";
 const QString binaryExtension = ".bin";
 const double truncationFactor = 0.75;
-const int fileNameLabelWidth = 200;
+const int fileNameLabelWidth = 320;
 const int fileSizeLabelWidth = 60;
-const int fileOwnerLabelWidth = 100;
+const int fileOwnerLabelWidth = 200;
+
+const QString previewIconPath = ":/images/eye-bold.svg";
+const QString shareIconPath = ":/images/share-fat-fill.svg";
+const QString deleteIconPath = ":/images/trash-fill.svg";
+const QString downloadIconPath =":/images/download-simple-bold.svg";
 
 //source: https://stackoverflow.com/questions/2053335/what-should-be-the-valid-characters-in-usernames
 constexpr std::string_view RESTRICTED_CHARS = R"(\/:*?"<>|'%;&=+$#@!~()[]{}., )";
+
+const int OWNED_FILES_PAGE_INDEX = 0;
+const int SHARED_FILES_PAGE_INDEX = 1;
 
 namespace Styles {
 const QString CentralWidget = R"(
@@ -21,28 +29,44 @@ const QString CentralWidget = R"(
         )";
 
 const QString FileItem = R"(
-            QWidget {
-                background-color: #E7ECEF;
-                border-bottom: 1px solid #8B8C89;
-                padding: 8px;
-            }
+    QWidget {
+        background-color: white;
+        border-bottom: 1px solid #BDBDBD;
+        padding: 8px 4px;
+        margin: 0;
+    }
 
-            QLabel {
-                color: #274C77;
-                font-weight: bold;
-            }
+    QLabel {
+        color: #424242;
+        background-color: transparent;
+        border: none;
+    }
 
-            QPushButton {
-                background-color: #6096BA;
-                color: white;
-                padding: 5px 10px;
-                border-radius: 4px;
-            }
+    QLabel#fileNameLabel {
+        font-weight: bold;
+    }
+)";
+const QString TransparentButton = R"(
+        QPushButton { 
+            border: none; 
+            background: transparent; 
+        }
+        QPushButton:hover { 
+            background-color: #F0F0F0; 
+            border-radius: 4px; 
+        }
+    )";
 
-            QPushButton:hover {
-                background-color: #A3CEF1;
-            }
-        )";
+const QString SelectedSidebarButton = R"(
+    background-color: #E3F2FD; 
+    font-weight: bold; 
+    color: #2196F3;
+)";
+
+const QString UnselectedSidebarButton = R"(
+    background-color: transparent; 
+    color: #424242;
+)";
 }
 
 
