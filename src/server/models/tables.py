88ea25f908dfb4f38e1d5989a293a71328a30953
jeancarson <jeanloucarson@gmail.com>
2025-06-03 22:59:38 +0100

    change to timeone aware timestamps

--- src/server/models/tables.py
+++ src/server/models/tables.py
@@ -16,9 +16,9 @@ class Users(Base):
     public_key = Column(String(191), nullable=False, unique=True) #Long term Identity Key
     spk = Column(String(191), nullable=False) #Signed Pre Key
     spk_signature = Column(String(191), nullable=False) #Signature of the Signed Pre Key
-    spk_updated_at = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    spk_updated_at = Column(DateTime(timezone=True), nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True), nullable=False)
 
     files = relationship(
         "Files",
@@ -51,8 +51,8 @@ class OTPK(Base):
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     key = Column(String(191), nullable=False) #A SIGNED pre key
     used = Column(Integer, nullable=False, default=0)  # 0 for unused, 1 for used
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True), nullable=False)
     
     user = relationship("Users", back_populates="otpks")
 
@@ -67,8 +67,8 @@ class FilePermissions(Base):
     encryption_key = Column(String(191), nullable=False) #the symmetric key, encrypted with derived shared secret
     otpk_id = Column(Integer, ForeignKey('one_time_pre_keys.id'), nullable=True) #The ID of the one-time pre key used for this permission
     ephemeral_key = Column(String(191), nullable=False) #The ephemeral key used for this permission    
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False)
+    updated_at = Column(DateTime(timezone=True), nullable=False)
 
     user = relationship("Users", back_populates="file_permissions")
     file = relationship("Files", back_populates="file_permissions")
@@ -82,7 +82,7 @@ class Files(Base):
     uuid = Column(String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()), index=True)
     name = Column(String(255), nullable=True)  # Can be null for files like .env
     path = Column(String(512), nullable=False)
-    uploaded_at = Column(DateTime, nullable=False, index=True)
+    uploaded_at = Column(DateTime(timezone=True), nullable=False, index=True)
     owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
     user = relationship("Users", back_populates="files")
@@ -110,7 +110,7 @@ class FileMetadata(Base):
         CheckConstraint('size <= 104857600', name='max_file_size_100mb'),
     )
     format = Column(String(50), nullable=False)
-    last_updated_at = Column(DateTime, nullable=False)
+    last_updated_at = Column(DateTime(timezone=True), nullable=False)
 
     file = relationship("Files", back_populates="file_metadata")
 
