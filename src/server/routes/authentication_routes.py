54b522296037d68d5dd2bc131ee110928c424a0c
jeancarson <jeanloucarson@gmail.com>
2025-06-05 14:17:13 +0100

    dont block login

--- src/server/routes/authentication_routes.py
+++ src/server/routes/authentication_routes.py
@@ -21,7 +21,11 @@ def login():
     Expected response:
     {
         "access_token": "<JWT_access_token>",
-        "refresh_token": "<JWT_refresh_token>"
+        "refresh_token": "<JWT_refresh_token>",
+        "spk_updated_at": "<ISO 8601 timestamp>",
+        "unused_otpk_count": "<number>",
+        "spk_outdated": "<boolean>",
+        "otpk_count_low": "<boolean>"
     }
     """
 
