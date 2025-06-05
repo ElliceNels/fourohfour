5c6bb43ca39aeadb1102fd99261f0cb734060c7f
jeancarson <jeanloucarson@gmail.com>
2025-06-05 01:04:37 +0100

    small fixes to get running

--- src/web-client/app.py
+++ src/web-client/app.py
@@ -252,4 +252,4 @@ def record_login_attempt(ip):
     login_attempts[ip] = attempts
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
